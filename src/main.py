30d5fc237a3122c16ba5fc5bbd786ae3bdfb27df
CarlosGoDo <cgomezdo22@alumnes.ub.edu>
2026-03-31 20:14:25 +0200

    links tablas

--- src/main.py
+++ src/main.py
@@ -3,17 +3,31 @@ from bs4 import BeautifulSoup as parse
 from datetime import datetime as hoy
 #from utils import extraer_marca, sleep_random, guardar_csv
 
-BASE_URL = "https://www.pccomponentes.com/portatiles"
 
-def get_product_links(page_url):
-    response = requests.get(page_url)
+
+def get_product_links(page_url, headers):
+    response = requests.get(page_url, headers=headers)
     response.raise_for_status()  # Raise an exception for bad status codes
-    soup = parse(response.text, "html.parser")
+
+    soup = parse(response.text, "lxml")
 
     links = []
-    for producto in soup.find_all("..."):
-        link = producto.find("a")["href"]
-        links.append(link)
+
+    # table = soup.find("table", class_="wikitable sortable jquery-tablesorter")
+    table = soup.find_all("table", class_="wikitable")
+
+    table_body = table[0].find("tbody")
+
+    rows = table_body.find_all("tr")[1:]
+    
+    for row in rows:
+        cells = row.find_all("td")[0]
+        if not cells:
+            continue
+
+        link_tag = cells.find("a", href=True, class_=False)
+        if link_tag:
+            links.append("https://es.wikipedia.org/"+link_tag["href"])
 
     return links
 
@@ -39,12 +53,12 @@ def parse_product(product_url):
     return data
 
 
+import requests
 
+url = "https://es.wikipedia.org/wiki/Provincia_(Espa%C3%B1a)"
 
-url = "https://www.pccomponentes.com/portatiles"
-r = requests.get(url, timeout=20)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0 Safari/537.36"
+}
 
-print(r.status_code)
-print(r.headers)
-print(r.text[:500])
-
+print(get_product_links(url, headers))
